44c29d5621da995cc62bf3a8cfee6a5fa444bf2f
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-04-23 16:55:45 +0300

    ...

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,29 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--branch=stable",
-		lazyrepo,
-		lazypath
-	})
-
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-
-vim.opt.rtp:prepend(lazypath)
-
 require("lazy").setup({
 	{
 		'phaazon/hop.nvim'
@@ -240,6 +214,11 @@ require("lazy").setup({
 	--
 	{
 		"mason-org/mason.nvim",
+		opts = {}
+	},
+
+	{
+		"nvim-tree/nvim-web-devicons",
 		opts = {}
 	}
 })
