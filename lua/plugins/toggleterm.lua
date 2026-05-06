bbcb6c44e7b04c4255c5c98b8bf7315687e2ff74
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-05-06 23:10:41 +0300

    ...

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,5 +3,28 @@ return {
 	version = "*",
 	opts = {
 		open_mapping = [[<A-0>]],
+		direction = "float",
+		-- direction = "horizontal",
+		-- direction = "tab",
+
+		float_opts = {
+			border = "single",
+		},
+		highlights = {
+			--[[
+			FloatBorder = {
+				guifg = "#e0e0e0",
+				guibg = "#000000",
+			},
+			--]]
+			Normal = {
+				guifg = "#e0e0e0",
+				guibg = "#000000",
+			},
+			NormalFloat = {
+				guifg = "#e0e0e0",
+				guibg = "#000000",
+			},
+		},
 	}
 }
