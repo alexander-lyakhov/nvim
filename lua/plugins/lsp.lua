544d337873918a930073912a86255d13d9c00a04
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-05-08 23:15:18 +0300

    Настроил LSP для Vue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,6 +34,73 @@ return {
 		'hrsh7th/cmp-nvim-lsp',
 	},
 	config = function()
+
+		-- LSP servers and clients are able to communicate to each other what features they support.
+		-- By default, Neovim doesn't support everything that is in the LSP specification.
+		-- When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+		-- So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+		-- Enable the following language servers
+		--
+		-- Add any additional override configuration in the following tables. Available keys are:
+		-- - cmd (table): Override the default command used to start the server
+		-- - filetypes (table): Override the default list of associated filetypes for the server
+		-- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+		-- - settings (table): Override the default settings passed when initializing the server.
+		local servers = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = 'Replace',
+						},
+						runtime = { version = 'LuaJIT' },
+						workspace = {
+							checkThirdParty = false,
+							library = vim.api.nvim_get_runtime_file('', true),
+						},
+						diagnostics = {
+							globals = { 'vim' },
+							disable = { 'missing-fields' },
+						},
+						format = {
+							enable = false,
+						},
+					},
+				},
+			},
+			-- @@@ LSP Servers
+			clangd = {},
+			jsonls = {},
+			sqlls = {},
+			yamlls = {},
+			bashls = {},
+			dockerls = {},
+			docker_compose_language_service = {},
+			-- tailwindcss = {},
+			-- graphql = {},
+			html = { filetypes = { 'html', 'twig', 'hbs', 'vue' } },
+			-- wc_ls = { filetypes = { 'html', 'vue', 'js', 'ts' } },
+			cssls = {},
+			ts_ls = {
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vim.fn.stdpath('data') .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+							languages = { 'javascript', 'typescript', 'vue' }
+						},
+					}
+				},
+				filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
+			},
+			vue_ls = {},
+			rust_analyzer = {},
+			pyright = {},
+		}
+
 		vim.api.nvim_create_autocmd('LspAttach', {
 			group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
 			-- Create a function that lets us more easily define mappings specific LSP related items.
@@ -122,6 +189,7 @@ return {
 						end,
 					})
 
+					
 					vim.api.nvim_create_autocmd('FileType', {
 						pattern = 'vue',
 						callback = function(args)
@@ -141,6 +209,7 @@ return {
 							})
 						end,
 					})
+					
 				end
 
 				-- The following code creates a keymap to toggle inlay hints in your
@@ -152,72 +221,6 @@ return {
 				end
 			end,
 		})
-
-		-- LSP servers and clients are able to communicate to each other what features they support.
-		-- By default, Neovim doesn't support everything that is in the LSP specification.
-		-- When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-		-- So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-		-- Enable the following language servers
-		--
-		-- Add any additional override configuration in the following tables. Available keys are:
-		-- - cmd (table): Override the default command used to start the server
-		-- - filetypes (table): Override the default list of associated filetypes for the server
-		-- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-		-- - settings (table): Override the default settings passed when initializing the server.
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = 'Replace',
-						},
-						runtime = { version = 'LuaJIT' },
-						workspace = {
-							checkThirdParty = false,
-							library = vim.api.nvim_get_runtime_file('', true),
-						},
-						diagnostics = {
-							globals = { 'vim' },
-							disable = { 'missing-fields' },
-						},
-						format = {
-							enable = false,
-						},
-					},
-				},
-			},
-			-- @@@ LSP Servers
-			clangd = {},
-			jsonls = {},
-			sqlls = {},
-			yamlls = {},
-			bashls = {},
-			dockerls = {},
-			docker_compose_language_service = {},
-			-- tailwindcss = {},
-			-- graphql = {},
-			html = { filetypes = { 'html', 'twig', 'hbs', 'vue' } },
-			-- wc_ls = { filetypes = { 'html', 'vue', 'js', 'ts' } },
-			cssls = {},
-			ts_ls = {
-				init_options = {
-					plugins = {
-						{
-							name = "@vue/typescript-plugin",
-							location = vim.fn.stdpath('data') .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-							languages = { 'javascript', 'typescript', 'vue' }
-						},
-					}
-				},
-				filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
-			},
-			vue_ls = {},
-			rust_analyzer = {},
-			pyright = {},
-		}
 
 		-- Ensure the servers and tools above are installed
 		local ensure_installed = vim.tbl_keys(servers or {})
