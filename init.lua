9065ad8b2858007d09009ea1fb5d288407e93a03
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-05-16 19:13:14 +0300

    ...

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ require("config.lazy")
 vim.cmd.colorscheme("catppuccin")
 -- vim.cmd.colorscheme("catppuccin-macchiato")
 
--- local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 
 --[[
@@ -64,3 +64,37 @@ vim.keymap.set("n", "<C-A-Up>", function()
 	end
 end, opts)
 --]]
+
+vim.keymap.set("n", "<A-f>", function()
+	vim.print('!!! test ++--')
+
+	local pattern = '^(%s*import)%s+(.*)%s*(from)%s+(.+)$'
+	local maxSize = 0
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	
+	for i, line in ipairs(lines) do
+		local res1, res2, res3, res4 = line:match(pattern)
+
+		if  res2 ~= nil then
+			res2 = res2:gsub('%s*$', '')
+			
+			if maxSize < #res2 then
+				maxSize = #res2
+			end
+		end
+	end
+
+	for i, line in ipairs(lines) do
+		local res1, res2, res3, res4 = line:match(pattern)
+
+		if res2 ~= nil then
+			res2 = res2:gsub('%s*$', '')
+			lines[i] = res1 .. ' ' .. string.format('%-' .. maxSize + 1 .. 's', res2) .. res3 .. ' ' .. res4, i
+		else
+			lines[i] = line
+		end
+	end
+
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+end, opts)
+
