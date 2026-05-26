dce4851f022441c2eb5ddc54419dd00f10a87f21
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-05-26 20:36:33 +0300

    ...

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,7 +90,7 @@ return {
 						{
 							name = "@vue/typescript-plugin",
 							location = vim.fn.stdpath('data') .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-							languages = { 'javascript', 'typescript', 'vue' }
+							languages = { 'javascript', 'typescript', 'vue', 'typescriptvue' }
 						},
 					}
 				},
